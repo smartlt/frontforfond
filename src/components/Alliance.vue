a033283e690588489afbb6259fb52a44becd2d0b
popobaza <nitidhero@outlook.com>
2018-10-27 11:01:15 +0700

    add getRows and getCols for alliance database mockup

--- src/components/Alliance.vue
+++ src/components/Alliance.vue
@@ -5,18 +5,33 @@
                 <h1 class="alliance-header">พันธมิตร</h1>
             </b-col>
         </b-row>
+        <p v-for="n in getRows">1</p>
+        
     </b-container>
 </template>
 
 <script>
 import allianceData from "./alliance-mockup.json";
-
+const defineColNumbers = 7;
 export default {
   name: "Alliance",
   data() {
     return {
       alliance: allianceData
     };
+  },
+  computed: {
+    getRows: function() {
+      let item = this.alliance.length;
+      let rows = parseInt(item / defineColNumbers);
+      if (item % defineColNumbers !== 0) {
+        rows = rows + 1;
+      }
+      return rows;
+    },
+    getCols: function() {
+      return defineColNumbers;
+    }
   }
 };
 </script>
